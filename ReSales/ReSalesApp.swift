e808eacdc6691722b3321d1f90fe70fae8ed40b5
moman <momanshaf@gmail.com>
2025-12-13 18:57:49 +0100

    Initial Commit

---
+++ ReSales/ReSalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReSalesApp.swift
+//  ReSales
+//
+//  Created by Moman Shafique on 13/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReSalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
